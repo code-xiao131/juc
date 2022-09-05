33982df65a126a1c3f7238da3756a7786710b226
XiaoCong <xiaocong@nineeyestech.com>
2022-09-05 22:20:46 +0800

    守护线程

--- src/main/java/com/juc/daemon/Test.java
+++ src/main/java/com/juc/daemon/Test.java
@@ -1,5 +1,16 @@
 package com.juc.daemon;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().isDaemon());
+            while (true) {
+
+            }
+        }, "t1");
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println(Thread.currentThread().getName() + "主线程结束------");
+    }
+
 }
